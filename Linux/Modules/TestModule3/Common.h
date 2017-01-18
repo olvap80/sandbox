856df2a62406ea5935ce4b986d05aa430b89a78f
Pavlo M <olvap80@gmail.com>
2017-01-18 07:39:10 +0200

    parameters, comments and naming

--- Linux/Modules/TestModule3/Common.h
+++ Linux/Modules/TestModule3/Common.h
@@ -9,11 +9,23 @@
  @param level corresponds to KERN_*level* as passed to printk (see
  http://lxr.free-electrons.com/source/include/linux/kern_levels.h for details).
  @param action what to do when condition failed (note: one has to do the most to
- restore/continue execution/bubble up the problem instead of causeing reset). */
+ restore/continue execution/bubble up the problem instead of causeing reset).
+ Sample usage:
+ @code
+	CHECKED( t < timeout, INFO, goto Error);
+
+	{
+	    int a = 10, b = 20;
+		CHECKED(a < b,INFO,printk(KERN_INFO "ACTION for a < b failure"));
+		CHECKED(a > b,INFO,printk(KERN_INFO "ACTION for a > b failure"));
+	}
+ @endcode*/
 #define CHECKED(cond, level, action) \
 	if( unlikely(!(cond)) ) \
 		{ printk(KERN_##level "%s:%d: %s: Assertion '%s' failed.\n", \
 		__FILE__,__LINE__,__func__, #cond); action; } \
 	else ((void)0)
 
-//CHECKED( t < timeout, INFO, goto Error);
+/*
+
+*/
--- Linux/Modules/TestModule3/InitAndExit.c
+++ Linux/Modules/TestModule3/InitAndExit.c
@@ -9,23 +9,28 @@ module_param(test_param1, int, 0644);
 static uint test_param2=0xFFFFFFFF;
 module_param(test_param2, uint, 0644);
 
+static int num_array_params = 0;
+static uint num_array[12];
+module_param_array(num_array, uint, &num_array_params, 0644);
 
-static int __init OnInitModule(void)
+
+static int __init on_init_module(void)
 {
-	printk(KERN_INFO "INIT_MODULE3 enter %d, %u\n",
-		test_param1, test_param2);
+	printk(KERN_INFO "INIT_MODULE3 enter %d, %u, %d\n",
+		test_param1, test_param2, num_array_params);
+	for(int i = 0; i < num_array_params; ++i){
+		printk(KERN_INFO "INIT_MODULE3 num_array[%d] = %u\n",
+			i, num_array[i]);
+	}
 
 	//TODO
-	{ int a = 10, b = 20;
-	  CHECKED(a < b,INFO,printk(KERN_INFO "ACTION a < b"));
-	  CHECKED(a > b,INFO,printk(KERN_INFO "ACTION a > b"));
-	}
 
 	printk(KERN_INFO "INIT_MODULE3 leave\n");
 	return 0;
 }
+module_init(on_init_module)
 
-static void __exit OnExitModule(void)
+static void __exit on_exit_module(void)
 {
 	printk(KERN_INFO "CLEANUP_MODULE3 enter\n");
 
@@ -34,9 +39,7 @@ static void __exit OnExitModule(void)
 	printk(KERN_INFO "CLEANUP_MODULE3 leave %d, %u\n",
 		test_param1, test_param2);
 }
-
-module_init(OnInitModule)
-module_exit(OnExitModule)
+module_exit(on_exit_module)
 
 
 MODULE_LICENSE("GPL");
@@ -45,5 +48,6 @@ MODULE_DESCRIPTION("Simple driver3");
 
 MODULE_PARM_DESC(test_param1, "Module test parameter 1");
 MODULE_PARM_DESC(test_param2, "Module test parameter 2");
+MODULE_PARM_DESC(num_array, "Module test parameter num_array");
 
 MODULE_SUPPORTED_DEVICE("TestDevice3");
